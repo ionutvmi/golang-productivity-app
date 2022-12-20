c4603bee74ab5d1c7370678a89de833fca4d989c
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-12-20 13:01:01 +0200

    Initial structure for the layout

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 func main() {
@@ -16,9 +17,8 @@ func main() {
 }
 
 type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
+	width  int
+	height int
 }
 
 func (m model) Init() tea.Cmd {
@@ -29,83 +29,51 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
-	// Is it a key press?
 	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
 		switch msg.String() {
-
-		// These keys should exit the program.
 		case "ctrl+c", "q":
 			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
 		}
+
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
 	}
 
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
 	return m, nil
 }
 
 func (m model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
+	var bordersWidth = 2
 
-	// Iterate over our choices
-	for i, choice := range m.choices {
+	var titleStyle = lipgloss.
+		NewStyle().
+		Width(m.width).
+		Padding(1).
+		Align(lipgloss.Center)
 
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
+	var title = titleStyle.Render("Productivity app")
 
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
+	var titleHeight = lipgloss.Height(title)
 
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
+	var panelStyle = lipgloss.
+		NewStyle().
+		Width(m.width/2-bordersWidth).
+		Height(m.height/2-bordersWidth-titleHeight/2).
+		BorderStyle(lipgloss.RoundedBorder()).
+		Align(lipgloss.Center, lipgloss.Center)
 
-	// The footer
-	s += "\nPress q to quit.\n"
+	var topPanels = lipgloss.JoinHorizontal(lipgloss.Top, panelStyle.Render("Hello top left"), panelStyle.Render("Hello top right"))
+	var bottomPanels = lipgloss.JoinHorizontal(lipgloss.Top, panelStyle.Render("Hello bottom left"), panelStyle.Render("Hello bottom right"))
+
+	var s string
+	s += lipgloss.JoinVertical(lipgloss.Left, title, topPanels, bottomPanels)
+	// s += strconv.Itoa(m.width) + " / " + strconv.Itoa(m.height)
 
 	// Send the UI for rendering
 	return s
 }
 
 func initialModel() model {
-	return model{
-		// Our to-do list is a grocery list
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
-
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
-	}
+	return model{}
 }
