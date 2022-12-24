e8c846e05549a31ffc04ff4e6d663ffc7fec5969
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-12-24 15:13:33 +0200

    Updates the build link flags

--- build.ps1
+++ build.ps1
@@ -1,4 +1,7 @@
 Write-Output "Generating build/app.exe ..."
+
+# -s Omit the symbol table and debug information.
+# -w Omit the DWARF symbol table.
 go build -ldflags "-w -s" -o build/app.exe main.go
 Write-Output "Done"
 
